c3b276a732f833189e2caac00f054b91667881f8
jperalta-2021586 <jperalta-2021586@kinal.edu.gt>
2022-09-02 22:44:32 -0600

    Se agregaron los datos de la tabla de MySQL a Reservacion.java  jperalta-2021586

--- MyHostelGT/src/java/modelo/Reservacion.java
+++ MyHostelGT/src/java/modelo/Reservacion.java
@@ -1,14 +1,74 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo;
 
-/**
- *
- * @author Grupo 3
- */
 public class Reservacion {
+    private int codigoReservacion;
+    private String fechaEntrada;
+    private String fechaSalida;
+    private int ID;
+    private int numHabitacion;
+    private int acceso;
+
+    public Reservacion() {
+    }
+
+    public Reservacion(int codigoReservacion, String fechaEntrada, String fechaSalida, int ID, int numHabitacion, int acceso) {
+        this.codigoReservacion = codigoReservacion;
+        this.fechaEntrada = fechaEntrada;
+        this.fechaSalida = fechaSalida;
+        this.ID = ID;
+        this.numHabitacion = numHabitacion;
+        this.acceso = acceso;
+    }
+
+    public int getCodigoReservacion() {
+        return codigoReservacion;
+    }
+
+    public void setCodigoReservacion(int codigoReservacion) {
+        this.codigoReservacion = codigoReservacion;
+    }
+
+    public String getFechaEntrada() {
+        return fechaEntrada;
+    }
+
+    public void setFechaEntrada(String fechaEntrada) {
+        this.fechaEntrada = fechaEntrada;
+    }
+
+    public String getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(String fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getNumHabitacion() {
+        return numHabitacion;
+    }
+
+    public void setNumHabitacion(int numHabitacion) {
+        this.numHabitacion = numHabitacion;
+    }
+
+    public int getAcceso() {
+        return acceso;
+    }
+
+    public void setAcceso(int acceso) {
+        this.acceso = acceso;
+    }
+    
+    
+    
     
 }
