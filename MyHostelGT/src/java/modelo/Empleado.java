63d50442b42e7dfabb83c07b777f2f63801e3d30
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2022-09-03 08:57:56 -0600

    Levante el login y realice el Validar, Empleado y EmpleadoDAO

--- MyHostelGT/src/java/modelo/Empleado.java
+++ MyHostelGT/src/java/modelo/Empleado.java
@@ -1,14 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo;
 
-/**
- *
- * @author Grupo 3
- */
 public class Empleado {
+    
+    private int acceso;
+    private String nombreEmpleado;
+    private String telefono;
+    private String usuario;
+    private int codigotipoEmpleado;
+
+    public Empleado() {
+    }
+
+    public Empleado(int acceso, String nombreEmpleado, String telefono, String usuario, int codigotipoEmpleado) {
+        this.acceso = acceso;
+        this.nombreEmpleado = nombreEmpleado;
+        this.telefono = telefono;
+        this.usuario = usuario;
+        this.codigotipoEmpleado = codigotipoEmpleado;
+    }
+
+    public int getAcceso() {
+        return acceso;
+    }
+
+    public void setAcceso(int acceso) {
+        this.acceso = acceso;
+    }
+
+    public String getNombreEmpleado() {
+        return nombreEmpleado;
+    }
+
+    public void setNombreEmpleado(String nombreEmpleado) {
+        this.nombreEmpleado = nombreEmpleado;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public int getCodigotipoEmpleado() {
+        return codigotipoEmpleado;
+    }
+
+    public void setCodigotipoEmpleado(int codigotipoEmpleado) {
+        this.codigotipoEmpleado = codigotipoEmpleado;
+    }
     
 }
