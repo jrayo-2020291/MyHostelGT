c6be6bf9e5f8b5658fcf62a67463463c3c80d369
imonterroso-2018253 <imonterroso-2018253@kinal.edu.gt>
2022-09-03 20:58:10 -0600

    Se modifico el MetodoDePago.java, MetodoDePagoDAO.java, MetodoDePago.jsp y el Principal tambien se agregaron imagenes de usuario

--- MyHostelGT/src/java/modelo/MetodoDePagoDAO.java
+++ MyHostelGT/src/java/modelo/MetodoDePagoDAO.java
@@ -1,14 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo;
 
-/**
- *
- * @author Grupo 3
- */
+import config.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MetodoDePagoDAO {
+    Conexion cn = new Conexion();
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
     
+    public List listar(){
+        String sql = "select * from metodoDePago";
+        List<MetodoDePago> listaMetodoDePago = new ArrayList<>();
+        try{
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                MetodoDePago MePa = new MetodoDePago();
+                MePa.setCodigoMetodoPago(rs.getInt(1));
+                MePa.setDescripcion(rs.getString(2));
+                listaMetodoDePago.add(MePa);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return listaMetodoDePago;        
+    }
 }
