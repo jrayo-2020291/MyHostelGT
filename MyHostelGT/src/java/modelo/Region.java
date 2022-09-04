00d700921110a0ddd64fc38a77ab03a2a808e4e0
jvasquez-2021117 <jvasquez-2021117@kinal.edu.gt>
2022-09-03 20:31:34 -0600

    Se modifico region.java, regionDAO.java y Region.jsp

--- MyHostelGT/src/java/modelo/Region.java
+++ MyHostelGT/src/java/modelo/Region.java
@@ -11,4 +11,33 @@ package modelo;
  */
 public class Region {
     
+    private int codigoRegion;
+    private String region;
+
+    public Region() {
+    }
+
+    public Region(int codigoRegion, String region) {
+        this.codigoRegion = codigoRegion;
+        this.region = region;
+    }
+
+    public int getCodigoRegion() {
+        return codigoRegion;
+    }
+
+    public void setCodigoRegion(int codigoRegion) {
+        this.codigoRegion = codigoRegion;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+    
+    
+    
 }
