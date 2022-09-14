8ffaa574d43e4774cdcdfcceb4e7abc461fd54dd
jrayo-2020291 <jrayo-2020291@kinal.edu.gt>
2022-09-13 23:51:26 -0600

    Se agregó el word

--- MyHostelGT/src/java/config/Conexion.java
+++ MyHostelGT/src/java/config/Conexion.java
@@ -10,7 +10,7 @@ public class Conexion {
         try{
             Class.forName("com.mysql.jdbc.Driver");
 
-            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/DBMyHostelGT?useSSL=false", "root", "admin");   
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/DBMyHostelGT?useSSL=false", "root", "19263");   
 
         }catch(Exception e){
             e.printStackTrace();
